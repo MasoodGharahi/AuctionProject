2f5e71a63f7804bfd7667170b9a2e70e227aa04f
MasoodGharahi <72644643+MasoodGharahi@users.noreply.github.com>
2024-01-08 21:29:04 +0330

    Identity server database switched to Postgres

--- IdentityService/HostingExtensions.cs
+++ IdentityService/HostingExtensions.cs
@@ -35,26 +35,30 @@ internal static class HostingExtensions
             .AddInMemoryApiScopes(Config.ApiScopes)
             .AddInMemoryClients(Config.Clients)
             .AddAspNetIdentity<ApplicationUser>();
+        builder.Services.ConfigureApplicationCookie(options =>
+        {
+            options.Cookie.SameSite = SameSiteMode.Lax;
+            });
 
         builder.Services.AddAuthentication();
-            //.AddGoogle(options =>
-            //{
-            //    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+        //.AddGoogle(options =>
+        //{
+        //    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
 
-            //    // register your IdentityServer with Google at https://console.developers.google.com
-            //    // enable the Google+ API
-            //    // set the redirect URI to https://localhost:5001/signin-google
-            //    options.ClientId = "copy client ID from Google here";
-            //    options.ClientSecret = "copy client secret from Google here";
-            //});
+        //    // register your IdentityServer with Google at https://console.developers.google.com
+        //    // enable the Google+ API
+        //    // set the redirect URI to https://localhost:5001/signin-google
+        //    options.ClientId = "copy client ID from Google here";
+        //    options.ClientSecret = "copy client secret from Google here";
+        //});
 
         return builder.Build();
     }
-    
+
     public static WebApplication ConfigurePipeline(this WebApplication app)
-    { 
+    {
         app.UseSerilogRequestLogging();
-    
+
         if (app.Environment.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
@@ -64,7 +68,7 @@ internal static class HostingExtensions
         app.UseRouting();
         app.UseIdentityServer();
         app.UseAuthorization();
-        
+
         app.MapRazorPages()
             .RequireAuthorization();
 
